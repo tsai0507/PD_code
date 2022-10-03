422c1b438a913ec7958add871aa366d8399e96b3
tsai0507 <frank4144@gmail.com>
2022-10-03 14:45:38 +0800

    first

---
+++ bev.py
@@ -0,0 +1,122 @@
+from re import X
+import cv2
+import numpy as np
+
+
+points = []
+
+class Projection(object):
+
+    def __init__(self, image_path, points):  #self.data_member :image,height,width,channels
+        """
+            :param points: Selected pixels on top view(BEV) image
+        """
+
+        if type(image_path) != str:
+            self.image = image_path
+        else:
+            self.image = cv2.imread(image_path)  #save img data
+        self.height, self.width, self.channels = self.image.shape
+        self.bev_uv=points
+
+    def top_to_front(self, theta=0, phi=0, gamma=0, dx=0, dy=0, dz=0, fov=np.pi/2):
+        """
+            Project the top view pixels to the front view pixels.
+            :return: New pixels on perspective(front) view image
+        """
+
+        ### TODO ###
+        f=0.5*512/(np.tan(fov/2)) #caculate focus length
+        #turn piexl coordinate to world  cooridnate             
+        z_bev=1
+        CORBEV=[]
+        for no in self.bev_uv:
+            CORBEV.append([z_bev*(no[0]-256)/f,
+                                                z_bev*(no[1]-256)/f,
+                                                z_bev,
+                                                1]) #CORBEV 4x4
+
+        #trans bev to front
+        rot=np.pi/2
+        transform=np.array([[1,0,0,0],
+                                                    [0,np.cos(rot),-np.sin(rot),0],
+                                                    [0,np.sin(rot),np.cos(rot),-1.5],
+                                                    [0,0,0,1]])
+        cor_bev=[]
+        for no in CORBEV:
+            cor_bev.append(
+                np.dot(transform,no)
+            )
+        #trans forn to uv
+        new_pixels=[]
+        for no in cor_bev:
+            new_pixels.append([int(-f*no[0]/no[2]+256),int(f*no[1]/no[2]+256)]) #CORBEV 4x4
+        # print(new_pixels)
+        return new_pixels
+
+    def show_image(self, new_pixels, img_name='projection.png', color=(0, 0, 255), alpha=0.4):
+        """
+            Show the projection result and fill the selected area on perspective(front) view image.
+        """
+        
+        new_image = cv2.fillPoly(
+            self.image.copy(), [np.array(new_pixels)], color)
+        new_image = cv2.addWeighted(
+            new_image, alpha, self.image, (1 - alpha), 0)
+  
+        # new_image=cv2.circle(self.image.copy(), new_pixels[0], 3, (0, 0, 255), -1)
+        # new_image=cv2.circle(new_image.copy(), new_pixels[1], 3, (0, 0, 255), -1)
+        # new_image=cv2.circle(new_image.copy(), new_pixels[2], 3, (0, 0, 255), -1)
+        # new_image=cv2.circle(new_image.copy(), new_pixels[3], 3, (0, 0, 255), -1)
+
+        cv2.imshow(
+            f'Top to front view projection {img_name}', new_image)
+        cv2.imwrite(img_name, new_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        return new_image
+
+
+def click_event(event, x, y, flags, params):
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        print(x, ' ', y)
+        points.append([x, y])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' + str(y), (x+5, y+5), font, 0.5, (0, 0, 255), 1)
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        cv2.imshow('image', img)
+
+    # checking for right mouse clicks
+    if event == cv2.EVENT_RBUTTONDOWN:
+
+        print(x, ' ', y)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' + str(g) + ',' + str(r), (x, y), font, 1, (255, 255, 0), 2)
+        cv2.imshow('image', img)
+
+
+if __name__ == "__main__":
+
+    pitch_ang = -90
+
+    front_rgb = "front_view_path.png"
+    top_rgb = "top_view_path.png"
+
+    # click the pixels on windo/w
+    img = cv2.imread(top_rgb, 1)
+    cv2.imshow('image', img)
+    cv2.setMouseCallback('image', click_event)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+   
+
+    projection = Projection(front_rgb, points)
+    new_pixels = projection.top_to_front(theta=pitch_ang)
+    print(new_pixels)
+    projection.show_image(new_pixels)
---
+++ load.py
@@ -0,0 +1,193 @@
+import numpy as np
+from PIL import Image
+import numpy as np
+import habitat_sim
+from habitat_sim.utils.common import d3_40_colors_rgb
+import cv2
+
+
+# This is the scene we are going to load.
+# support a variety of mesh formats, such as .glb, .gltf, .obj, .ply
+### put your scene path ###
+#test_scene = "replica_v1/apartment_0/habitat/mesh_semantic.ply"
+test_scene = "apartment_0/habitat/mesh_semantic.ply"
+
+sim_settings = {
+    "scene": test_scene,  # Scene path
+    "default_agent": 0,  # Index of the default agent
+    "sensor_height":1 ,  # Height of sensors in meters, relative to the agent1.5
+    "width": 512,  # Spatial resolution of the observations
+    "height": 512,
+    "sensor_pitch": 0,  # sensor pitch (x rotation in rads)(- is down,+is up)
+}
+
+
+# This function generates a config for the simulator.
+# It contains two parts:
+# one for the simulator backend
+# one for the agent, where you can attach a bunch of sensors
+
+def transform_rgb_bgr(image):
+    return image[:, :, [2, 1, 0]]
+
+def transform_depth(image):
+    depth_img = (image / 10 * 255).astype(np.uint8) #0~255
+    return depth_img
+
+def transform_semantic(semantic_obs):
+    semantic_img = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
+    semantic_img.putpalette(d3_40_colors_rgb.flatten())
+    semantic_img.putdata((semantic_obs.flatten() % 40).astype(np.uint8))
+    semantic_img = semantic_img.convert("RGB")
+    semantic_img = cv2.cvtColor(np.asarray(semantic_img), cv2.COLOR_RGB2BGR)
+    return semantic_img
+
+def make_simple_cfg(settings):
+    # simulator backend
+    sim_cfg = habitat_sim.SimulatorConfiguration()
+    sim_cfg.scene_id = settings["scene"]
+    # agent
+    agent_cfg = habitat_sim.agent.AgentConfiguration()
+
+    # In the 1st example, we attach only one sensor,
+    # a RGB visual sensor, to the agent
+    rgb_sensor_spec = habitat_sim.CameraSensorSpec()
+    rgb_sensor_spec.uuid = "color_sensor"
+    rgb_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    rgb_sensor_spec.resolution = [settings["height"], settings["width"]]
+    rgb_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    rgb_sensor_spec.orientation = [
+        settings["sensor_pitch"],
+        0,
+        0.0,
+    ]
+    rgb_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    
+    # a BEV_RGB visual sensor, to the agent
+    bev_rgb_sensor_spec = habitat_sim.CameraSensorSpec()
+    bev_rgb_sensor_spec.uuid = "bev_color_sensor"
+    bev_rgb_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    bev_rgb_sensor_spec.resolution = [settings["height"], settings["width"]]
+    bev_rgb_sensor_spec.position = [0.0, settings["sensor_height"], -1.5]
+    bev_rgb_sensor_spec.orientation = [
+        -np.pi/2,
+        0.0,
+        0.0,
+    ]
+    bev_rgb_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    
+
+    #depth snesor
+    depth_sensor_spec = habitat_sim.CameraSensorSpec()
+    depth_sensor_spec.uuid = "depth_sensor"
+    depth_sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
+    depth_sensor_spec.resolution = [settings["height"], settings["width"]]
+    depth_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    depth_sensor_spec.orientation = [
+        settings["sensor_pitch"],
+        0.0,
+        0.0,
+    ]
+    depth_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+
+    #semantic snesor
+    semantic_sensor_spec = habitat_sim.CameraSensorSpec()
+    semantic_sensor_spec.uuid = "semantic_sensor"
+    semantic_sensor_spec.sensor_type = habitat_sim.SensorType.SEMANTIC
+    semantic_sensor_spec.resolution = [settings["height"], settings["width"]]
+    semantic_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    semantic_sensor_spec.orientation = [
+        settings["sensor_pitch"],
+        0.0,
+        0.0,
+    ]
+    semantic_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+
+    agent_cfg.sensor_specifications = [rgb_sensor_spec,bev_rgb_sensor_spec, depth_sensor_spec, semantic_sensor_spec]
+
+    return habitat_sim.Configuration(sim_cfg, [agent_cfg])
+
+
+cfg = make_simple_cfg(sim_settings)
+sim = habitat_sim.Simulator(cfg)
+
+
+# initialize an agent
+agent = sim.initialize_agent(sim_settings["default_agent"])
+
+# Set agent state
+agent_state = habitat_sim.AgentState()
+agent_state.position = np.array([0.0, 0.0, 0.0])  # agent in world space
+agent.set_state(agent_state)
+
+# obtain the default, discrete actions that an agent can perform
+# default action space contains 3 actions: move_forward, turn_left, and turn_right
+action_names = list(cfg.agents[sim_settings["default_agent"]].action_space.keys())
+print("Discrete action space: ", action_names)
+
+
+FORWARD_KEY="w"
+LEFT_KEY="a"
+RIGHT_KEY="d"
+SAVE_FRONT="z"
+SAVE_BEV="x"
+FINISH="f"
+print("#############################")
+print("use keyboard to control the agent")
+print(" w for go forward  ")
+print(" a for turn left  ")
+print(" d for trun right  ")
+print(" f for finish and quit the program")
+print("#############################")
+
+
+def navigateAndSee(action=""):
+    if action in action_names:
+        observations = sim.step(action)
+        #print("action: ", action)
+
+        cv2.imshow("RGB", transform_rgb_bgr(observations["color_sensor"]))
+        cv2.imshow("BEV_RGB", transform_rgb_bgr(observations["bev_color_sensor"]))
+        #cv2.imshow("depth", transform_depth(observations["depth_sensor"]))
+        #cv2.imshow("semantic", transform_semantic(observations["semantic_sensor"]))
+        agent_state = agent.get_state()
+        sensor_state = agent_state.sensor_states['color_sensor']
+        print("camera pose: x y z rw rx ry rz")
+        print(sensor_state.position[0],sensor_state.position[1],sensor_state.position[2],  sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z)
+        return transform_rgb_bgr(observations["color_sensor"]) ,transform_rgb_bgr(observations["bev_color_sensor"])
+
+
+def save_front(save_img):
+    cv2.imwrite('front_view_path.png',save_img[0])
+
+def save_bev(save_img):
+    cv2.imwrite('top_view_path.png',save_img[1])
+
+action = "move_forward"
+save_img=navigateAndSee(action)
+
+while True:
+    keystroke = cv2.waitKey(0) #等待按鍵事件
+    if keystroke == ord(FORWARD_KEY): #ord()取得char得ASCII
+        action = "move_forward"
+        save_img=navigateAndSee(action)
+        print("action: FORWARD")
+    elif keystroke == ord(LEFT_KEY):
+        action = "turn_left"
+        save_img=navigateAndSee(action)
+        print("action: LEFT")
+    elif keystroke == ord(RIGHT_KEY):
+        action = "turn_right"
+        save_img=navigateAndSee(action)
+        print("action: RIGHT")
+    elif keystroke ==ord(SAVE_FRONT):
+        print("action: SAVE_IMG")
+        save_front(save_img)
+        save_bev(save_img)
+        break
+    elif keystroke == ord(FINISH):
+        print("action: FINISH")
+        break
+    else:
+        print("INVALID KEY")
+        continue
